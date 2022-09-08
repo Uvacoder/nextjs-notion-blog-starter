174b0f08a236c6f24ac413e02c0b694c9ba2a5d8
Tuan Phung <tuan@phung.cz>
2022-09-09 01:00:14 +0200

    fix

--- components/HeroHeader.tsx
+++ components/HeroHeader.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import siteData from 'data/siteData';
 
-export default function HeroHeader() {
+export default function HeroHeader({ blog }: any) {
+  console.log(blog?.profileUrl);
   return (
     <div className="py-16 text-center bg-gray-100">
-      {siteData?.profileUrl && (
+      {blog?.profileUrl && (
         <Image
-          src={siteData.profileUrl}
+          src={blog.profileUrl}
           className="mx-auto rounded-full"
           width={96}
           height={96}
@@ -14,10 +15,10 @@ export default function HeroHeader() {
         />
       )}
       <div className="mt-4 text-3xl font-extrabold text-gray-900">
-        {siteData.headerTitle}
+        {blog?.headerTitle}
       </div>
       <div className="max-w-2xl mx-auto mt-2 text-xl text-gray-500">
-        {siteData.headerDescription}
+        {blog?.headerDescription}
       </div>
     </div>
   );
--- data/siteData.ts
+++ data/siteData.ts
@@ -7,7 +7,7 @@ const siteData = {
   footerText: '© All rights reserved',
   language: 'en-us',
   locale: 'en-US',
-  socialBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
+  ogBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
   github: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
   twitter: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
   websiteUrl: 'https://nextjs-notion-blog-starter.vercel.app',
